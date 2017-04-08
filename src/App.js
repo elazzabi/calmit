e117f91c86e8b50f4dff1185e5bef2bee7a6b38a
Ahmed <elazzabi.ahmed@gmail.com>
2017-04-08 14:25:47 +0100

    Add tap events

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Grid from './Grid';
 
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
+
 class App extends Component {
   render() {
     return (
